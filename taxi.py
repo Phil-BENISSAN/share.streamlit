d0c79c04f3bb2256ad6133ee18e4bd49d5ab5359
Phil-BENISSAN <144322911+Phil-BENISSAN@users.noreply.github.com>
2024-11-19 18:23:55 +0100

    Add files via upload

---
+++ taxi.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+
+
+# Charger le dataset "taxis"
+df_taxis = sns.load_dataset('taxis')
+
+st.title('Welcome to WCS Taxi driver')
+
+
+list_pickup_borough = sorted(list(df_taxis[df_taxis['pickup_borough'].notna()]['pickup_borough'].unique()))
+
+
+choice_pickup_borough = st.selectbox(  "Enter your pickup area :", 
+                                       list_pickup_borough
+                                    )
+
+st.write(f'You are choosen : {choice_pickup_borough}')
+
+# AFFICHAGE DE L'IMAGE
+st.image(f'img\{choice_pickup_borough}.png',
+         use_column_width=True,
+         )
+
+
+
